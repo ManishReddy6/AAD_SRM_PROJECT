b34c7160e07d10ff1ac013a84e5269a1857d40d7
manish reddy <manishreddyalugonda@gmail.com>
2020-07-06 19:42:14 +0530

    file added

---
+++ Contextmenu/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.contextmenu;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button g1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        g1=findViewById(R.id.context);
+        registerForContextMenu(g1);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater m=getMenuInflater();
+        m.inflate(R.menu.menu,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.home:
+                Toast.makeText(MainActivity.this,"Home",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.note:
+                Toast.makeText(MainActivity.this,"Notifications",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.out:
+                AlertDialog.Builder dialog=new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("Alert Dialog");
+                dialog.setMessage("Do you really want to exit");
+                dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i){
+                        finish();
+                    }
+                });
+                dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+                dialog.setCancelable(false);
+                dialog.show();
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+}
